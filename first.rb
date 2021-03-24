7a2d87d6292859eef3ec17211d3209736299aeef
unknown <nhussaini@luc.edu>
2021-03-24 08:44:47 -0700

    added class and initialize method

--- first.rb
+++ first.rb
@@ -63,4 +63,20 @@ end
 =end
 names.each do|friend|
   puts friend
-end 
+end
+
+#classes and objects in ruby
+#in ruby everything is an object
+class Book
+  attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author= author
+    @pages = pages
+  end
+end
+
+book1 =Book.new("Harry Potter", "JK Rowling", 400)
+book2 =Book.new("Lord of the rings", "Tolkein",350)
+
+puts book1.author
